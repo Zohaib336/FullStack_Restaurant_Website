a43954123dc48d6f35be45a5d83dd8980b8d253c
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-09-21 22:13:11 +0530

    build Signup page

--- client/src/auth/Login.tsx
+++ client/src/auth/Login.tsx
@@ -2,13 +2,37 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Separator } from "@/components/ui/separator";
 import { Loader2, LockKeyhole, Mail } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
 import { Link } from "react-router-dom";
 
+// There are two methods of define type in typescript
+
+type LoginInputState = {
+    email: string;
+    password: string;
+}
+
 const Login = () => {
+    const [input, setInput] = useState<LoginInputState>({
+        email: "",
+        password: "",
+    });
+
+    const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setInput({ ...input, [name]: value });
+    };
+
+    const loginSubmitHandler = async (e: FormEvent) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
     const loading = false;
+
     return (
         <div className="flex items-center justify-center min-h-screen">
-            <form className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
+            <form onSubmit={loginSubmitHandler} className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
                 <div className="mb-4">
                     <h1 className="font-bold text-2xl">BiteMate</h1>
                 </div>
@@ -19,6 +43,8 @@ const Login = () => {
                             type="email"
                             placeholder="Email"
                             name="email"
+                            value={input.email}
+                            onChange={changeEventHandler}
                             className="pl-10 focus-visible:ring-1"
                         />
                         <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
@@ -31,6 +57,8 @@ const Login = () => {
                             type="password"
                             placeholder="Password"
                             name="password"
+                            value={input.password}
+                            onChange={changeEventHandler}
                             className="pl-10 focus-visible:ring-1"
                         />
                         <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
--- client/src/auth/Signup.tsx
+++ client/src/auth/Signup.tsx
@@ -1,7 +1,123 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { Separator } from "@/components/ui/separator";
+import { Loader2, LockKeyhole, Mail, PhoneIcon, User } from "lucide-react";
+import { ChangeEvent, FormEvent, useState } from "react";
+import { Link } from "react-router-dom";
+
+// There are two methods of define type in typescript
+
+type SignupInputState = {
+    fullname: string;
+    email: string;
+    password: string;
+    contact: string;
+}
+
 const Signup = () => {
+    const [input, setInput] = useState<SignupInputState>({
+        fullname: "",
+        email: "",
+        password: "",
+        contact: "",
+    });
+
+    const changeEventHandler = (e: ChangeEvent<HTMLInputElement>) => {
+        const { name, value } = e.target;
+        setInput({ ...input, [name]: value });
+    };
+
+    const loginSubmitHandler = async (e: FormEvent) => {
+        e.preventDefault();
+        console.log(input);
+    }
+
+    const loading = false;
+
     return (
-        <div>Signup</div>
+        <div className="flex items-center justify-center min-h-screen">
+            <form onSubmit={loginSubmitHandler} className="md:p-8 w-full max-w-md rounded-lg md:border border-gray-200 mx-4">
+                <div className="mb-4">
+                    <h1 className="font-bold text-2xl">BiteMate</h1>
+                </div>
+
+                <div className="mb-4">
+                    <div className="relative">
+                        <Input
+                            type="text"
+                            placeholder="Enter your full name"
+                            name="fullname"
+                            value={input.fullname}
+                            onChange={changeEventHandler}
+                            className="pl-10 focus-visible:ring-1"
+                        />
+                        <User className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                    </div>
+                </div>
+
+                <div className="mb-4">
+                    <div className="relative">
+                        <Input
+                            type="email"
+                            placeholder="Enter your email"
+                            name="email"
+                            value={input.email}
+                            onChange={changeEventHandler}
+                            className="pl-10 focus-visible:ring-1"
+                        />
+                        <Mail className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                    </div>
+                </div>
+
+                <div className="mb-4">
+                    <div className="relative">
+                        <Input
+                            type="password"
+                            placeholder="Create a strong password"
+                            name="password"
+                            value={input.password}
+                            onChange={changeEventHandler}
+                            className="pl-10 focus-visible:ring-1"
+                        />
+                        <LockKeyhole className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                    </div>
+                </div>
+
+                <div className="mb-4">
+                    <div className="relative">
+                        <Input
+                            type="text"
+                            placeholder="Enter your contact number"
+                            name="contact"
+                            value={input.contact}
+                            onChange={changeEventHandler}
+                            className="pl-10 focus-visible:ring-1"
+                        />
+                        <PhoneIcon className="absolute inset-y-2 left-2 text-gray-500 pointer-events-none" />
+                    </div>
+                </div>
+
+                <div className="mb-10">
+                    {loading ? (
+                        <Button disabled className="w-full bg-orange hover:bg-hoverOrange">
+                            <Loader2 className="mr-2 h-4 w-4 animate-spin" /> Please wait
+                        </Button>
+                    ) : (
+                        <Button
+                            type="submit"
+                            className="w-full bg-orange hover:bg-hoverOrange">
+                            Signup
+                        </Button>
+                    )}
+                </div>
+                <Separator />
+                <p className="mt-2">
+                    Already have an account?{" "}
+                    <Link to="/login" className="text-blue-500">Login</Link>
+                </p>
+            </form>
+        </div>
     )
 }
 
-export default Signup;
+export default Signup
