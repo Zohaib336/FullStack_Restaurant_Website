d8cb87ec8955f05e49a84c7a8b86cadbcece82d4
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-09-26 23:47:08 +0530

    build navbar

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,21 @@
 import './App.css'
 import Login from './auth/Login'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-import MainLayout from './MainLayout'
+// import MainLayout from './MainLayout'
 import Signup from './auth/Signup'
 import ForgotPassword from './auth/ForgotPassword'
 import ResetPassword from './auth/ResetPassword'
 import VerifyEmail from './auth/VerifyEmail'
+import Navbar from './components/Navbar'
 
 const appRouter = createBrowserRouter([
+  // {
+  //   path: "/",
+  //   element: <MainLayout />,
+  // },
   {
     path: "/",
-    element: <MainLayout />,
+    element: <Navbar />,
   },
   {
     path: "/login",
