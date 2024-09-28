4b64ad76de42639bbbf20cc1ba536782339d5884
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-09-28 12:53:58 +0530

    HeroSection build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,21 +1,24 @@
 import './App.css'
 import Login from './auth/Login'
 import { createBrowserRouter, RouterProvider } from "react-router-dom"
-// import MainLayout from './MainLayout'
 import Signup from './auth/Signup'
 import ForgotPassword from './auth/ForgotPassword'
 import ResetPassword from './auth/ResetPassword'
 import VerifyEmail from './auth/VerifyEmail'
-import Navbar from './components/Navbar'
+import HeroSection from './components/HeroSection'
+import MainLayout from './layout/MainLayout'
 
 const appRouter = createBrowserRouter([
-  // {
-  //   path: "/",
-  //   element: <MainLayout />,
-  // },
+
   {
     path: "/",
-    element: <Navbar />,
+    element: <MainLayout />,
+    children: [
+      {
+        path: "/",
+        element: <HeroSection />
+      },
+    ]
   },
   {
     path: "/login",
