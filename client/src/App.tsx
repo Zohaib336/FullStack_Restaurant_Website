2ab1eda4b3661242f00b759e70c4b44047f98ddd
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-10-09 00:57:11 +0530

    Admin's restaurant and menu page build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -11,6 +11,8 @@ import Profile from './components/Profile'
 import SearchPage from './components/SearchPage'
 import RestaurantDetail from './components/RestaurantDetail'
 import Cart from './components/Cart'
+import Restaurant from './admin/Restaurant'
+import AddMenu from './admin/AddMenu'
 
 const appRouter = createBrowserRouter([
 
@@ -37,6 +39,15 @@ const appRouter = createBrowserRouter([
       {
         path: "/cart",
         element: <Cart />
+      },
+      // Admin Services start from here
+      {
+        path: "/admin/restaurant",
+        element: <Restaurant />
+      },
+      {
+        path: "/admin/menu",
+        element: <AddMenu />
       }
     ]
   },
