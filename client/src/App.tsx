58edc81f50135b388143e1b1db9026307adf1da3
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-10-09 23:18:38 +0530

    admin orders page build

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -13,6 +13,7 @@ import RestaurantDetail from './components/RestaurantDetail'
 import Cart from './components/Cart'
 import Restaurant from './admin/Restaurant'
 import AddMenu from './admin/AddMenu'
+import Orders from './admin/Order'
 
 const appRouter = createBrowserRouter([
 
@@ -48,6 +49,10 @@ const appRouter = createBrowserRouter([
       {
         path: "/admin/menu",
         element: <AddMenu />
+      },
+      {
+        path: "/admin/orders",
+        element: <Orders />
       }
     ]
   },
