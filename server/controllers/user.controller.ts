f6ace75ae4bd754b7ad56c908acc055ffd1393ef
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-10-17 23:44:03 +0530

    cloudinary setup

--- server/controllers/user.controller.ts
+++ server/controllers/user.controller.ts
@@ -2,6 +2,9 @@ import { Request, Response } from "express";
 import { User } from "../models/user.model";
 import bcrypt from "bcryptjs";
 import crypto from "crypto";
+import { Date } from "mongoose";
+import cloudinary from "../utils/cloudinary";
+import mongoose from "mongoose";
 
 export const signup = async (req: Request, res: Response) => {
     try {
@@ -178,11 +181,41 @@ export const resetPassword = async (req: Request, res: Response) => {
 
 export const checkAuth = async (req: Request, res: Response) => {
     try {
-        // const userId = req.id;
+        const userId = req.id;
+        const user = await User.findById(userId).select("-password");
+        if (!user) {
+            return res.status(404).json({
+                success: false,
+                message: 'User not found'
+            });
+        };
+        return res.status(200).json({
+            success: true,
+            user
+        });
     } catch (error) {
         console.error(error);
-        return res.status(500).json({
-            message: "Internal server error"
-        });
+        return res.status(500).json({ message: "Internal server error" });
     }
 };
+export const updateProfile = async (req: Request, res: Response) => {
+    try {
+        const userId = req.id;
+        const { fullname, email, address, city, country, profilePicture } = req.body;
+        // Upload image on cloudinary
+        let cloudResponse: any;
+        cloudResponse = await cloudinary.uploader.upload(profilePicture);
+        const updatedData = { fullname, email, address, city, country, profilePicture };
+
+        const user = await User.findByIdAndUpdate(userId, updatedData, { new: true }).select("-password");
+
+        return res.status(200).json({
+            success: true,
+            user,
+            message: "Profile updated successfully"
+        });
+    } catch (error) {
+        console.error(error);
+        return res.status(500).json({ message: "Internal server error" });
+    }
+}
