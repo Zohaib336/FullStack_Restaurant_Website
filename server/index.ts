b94bd47599d65f21770194636364b17bb0e47b98
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-10-20 19:13:30 +0530

    routes for user

--- server/index.ts
+++ server/index.ts
@@ -1,11 +1,29 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./db/connectDB";
+import userRoute from "./routes/user.route";
+import bodyParser from "body-parser";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 
 dotenv.config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
+
+// Default middleware 
+app.use(bodyParser.json({ limit: '10mb' }));
+app.use(express.urlencoded({ extended: true, limit: '10mb' }));
+app.use(express.json());
+app.use(cookieParser());
+const corsOptions = {
+  origin: "http://localhost:5173",
+  credentials: true
+}
+app.use(cors(corsOptions));
+
+// Api
+app.use("/api/v1/user", userRoute);
 
 app.listen(PORT, () => {
   connectDB();
