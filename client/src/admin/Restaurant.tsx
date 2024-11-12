d052dcb1d26def10ff36c8a0441c414c1d68d00c
Zohaib Akhtar <zohaibakhtar336@gmail.com>
2024-11-12 21:37:21 +0530

    create restaurant zustand store

--- client/src/admin/Restaurant.tsx
+++ client/src/admin/Restaurant.tsx
@@ -2,8 +2,9 @@ import { Button } from "@/components/ui/button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
 import { RestaurantFormSchema, restaurantFromSchema } from "@/schema/restaurantSchema";
+import { useRestaurantStore } from "@/store/useRestaurantStore";
 import { Loader2 } from "lucide-react";
-import { FormEvent, useState } from "react";
+import { FormEvent, useEffect, useState } from "react";
 
 
 const Restaurant = () => {
@@ -18,6 +19,8 @@ const Restaurant = () => {
 
 
     const [errors, setErrors] = useState<Partial<RestaurantFormSchema>>({});
+    const { loading, restaurant, updateRestaurant, createRestaurant, getRestaurant } = useRestaurantStore();
+
     const changeEventHandler = (e: React.ChangeEvent<HTMLInputElement>) => {
         const { name, value, type } = e.target;
         setInput({ ...input, [name]: type === "number" ? Number(value) : value });
@@ -33,11 +36,46 @@ const Restaurant = () => {
             return;
         }
         // Add restaurant api implementation start from here
-        console.log(input)
+        try {
+            const formData = new FormData();
+            formData.append("restaurantName", input.restaurantName);
+            formData.append("city", input.city);
+            formData.append("country", input.country);
+            formData.append("deliveryTime", input.deliveryTime.toString());
+            formData.append("cuisines", JSON.stringify(input.cuisines));
+
+            if (input.imageFile) {
+                formData.append("imageFile", input.imageFile);
+            }
+
+            if (restaurant) {
+                // update
+                await updateRestaurant(formData);
+            } else {
+                // create
+                await createRestaurant(formData);
+            }
+        } catch (error) {
+            console.log(error);
+        }
     };
 
-    const loading = false;
-    const restaurant = false
+    useEffect(() => {
+        const fetchRestaurant = async () => {
+            await getRestaurant();
+            setInput({
+                restaurantName: restaurant.restaurantName || "",
+                city: restaurant.city || "",
+                country: restaurant.country || "",
+                deliveryTime: restaurant.deliveryTime || 0,
+                cuisines: restaurant.cuisines
+                    ? restaurant.cuisines.map((cuisine: string) => cuisine)
+                    : [],
+                imageFile: undefined,
+            });
+        };
+        fetchRestaurant();
+    }, [])
 
     return (
         <div className="max-w-6xl mx-auto my-10">
---
+++ client/src/store/useRestaurantStore.ts
@@ -0,0 +1,84 @@
+import axios from "axios";
+import { toast } from "sonner";
+import { create } from "zustand";
+import { createJSONStorage, persist } from "zustand/middleware";
+
+const API_END_POINT = "http://localhost:8000/api/v1/restaurant";
+axios.defaults.withCredentials = true;
+
+export const useRestaurantStore = create<any>(persist((set) => ({
+    loading: false,
+    restaurant: null,
+    searchedRestaurant: null,
+    createRestaurant: async (formData: FormData) => {
+        try {
+            set({ loading: true });
+            const response = await axios.post(`${API_END_POINT}/`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            if (response.data.success) {
+                toast.success(response.data.message);
+                set({ loading: false });
+            }
+        } catch (error: any) {
+            toast.error(error.response.data.message);
+            set({ loading: false });
+        }
+    },
+    getRestaurant: async () => {
+        try {
+            set({ loading: true });
+            const response = await axios.get(`${API_END_POINT}/`);
+            if (response.data.success) {
+                set({ loading: false, restaurant: response.data.restaurant });
+            }
+        } catch (error: any) {
+            if (error.response.status === 404) {
+                set({ restaurant: null });
+            }
+            set({ loading: false });
+        }
+    },
+    updateRestaurant: async (formData: FormData) => {
+        try {
+            set({ loading: true });
+            const response = await axios.put(`${API_END_POINT}/`, formData, {
+                headers: {
+                    'Content-Type': 'multipart/form-data'
+                }
+            });
+            if (response.data.success) {
+                toast.success(response.data.message);
+                set({ loading: false });
+            }
+        } catch (error: any) {
+            toast.error(error.response.data.message);
+            set({ loading: false });
+        }
+    },
+    searchRestaurant: async (searchText: string, searchQuery: string, selectedCuisines: any) => {
+        try {
+            set({ loading: true });
+
+            const params = new URLSearchParams();
+            params.set("searchQuery", searchQuery);
+            params.set("selectedCuisines", selectedCuisines.join(","));
+
+            // await new Promise((resolve) => setTimeout(resolve, 2000));
+            const response = await axios.get(`${API_END_POINT}/search/${searchText}?${params.toString()}`);
+            if (response.data.success) {
+                set({ loading: false, searchedRestaurant: response.data });
+            }
+        } catch (error) {
+            set({ loading: false });
+        }
+    },
+}),
+    {
+        name: "restaurant-name",
+        storage: createJSONStorage(() => localStorage)
+    }
+)
+);
